72aa256e3d32486a1045b5ce2fc1a0dc2b57027f
rickulinio <rickulinio@gmail.com>
2026-05-27 21:11:01 +0200

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,56 +1,6 @@
 const FACTIONS = [
 
   {
-    key: 'podanienafirme', 
-    name: 'FIRMA',
-    icon: '🏬',
-    color: '#caa35e',
-    desc: "Załóż firmę tutaj!",
-
-    webhook: 'https://discord.com/api/webhooks/1509270202318192640/7WTh4gmZDL2MBwWkR8EyUf-GQ-bMAKrcjepngr7B5Q5e-EsWc8T9tKx7Py_ktlYWJv96',
-    roleId: '1480310121707077753',
-
-    questions: [
-      {
-        section: 'OOC',
-        items: [
-          {
-            id: 'firma1',
-            label: 'Ile masz lat',
-            type: 'input',
-            required: true,
-            maxLength: 2
-          },
-          {
-            id: 'firma2',
-            label: 'Ile masz przegranych godzin w fivem:',
-            type: 'input',
-            required: true,
-          },
-          {
-            id: 'firma3',
-            label: 'Ile posiadasz ludzi:',
-            type: 'input',
-            required: true,
-          },
-          {
-            id: 'firma4',
-            label: 'Jaka firma:',
-            type: 'input',
-            required: true,
-          },
-          {
-            id: 'firma5',
-            label: 'Plan na firmę:',
-            type: 'textarea',
-            required: true,
-          },
-        ]
-      },
-    ]
-  },
-
-  {
     key: 'adm', 
     name: 'ADMINISTRACJA',
     icon: '🚨',
@@ -117,195 +67,6 @@ const FACTIONS = [
       },
     ]
   },
-
-  {
-    key: 'test', 
-    name: 'LSSD',
-    icon: '🛡️',
-    color: '#2dd4bf',
-    desc: "Los Santos Sheriff's Department. Ochrona spokoju poza miastem.",
-
-    webhook: 'https://discord.com/api/webhooks/1506332001198936098/U999ruVpvlqP5xpvpL3Bil5l0JRZtUPqAu8jSfEVuBGZ9tRfEteFGv-Vm6Vuxce4DzGe',
-    roleId: '1480310711878946916',
-
-    questions: [
-      {
-        section: 'OOC',
-        items: [
-          {
-            id: '2',
-            label: 'Wiek',
-            type: 'input',
-            required: true,
-            maxLength: 2
-          },
-          {
-            id: '3',
-            label: 'Imię',
-            type: 'input',
-            required: true
-          },
-          {
-            id: '4a',
-            label: 'Doświadczenie w frakcji LEA',
-            type: 'input',
-            required: true
-          },
-          {
-            id: '4b',
-            label: 'Na jakich serwerach grałeś',
-            type: 'input',
-            required: true
-          },
-          {
-            id: '4c',
-            label: 'Oceń swoją wiedze w skali od 1-10',
-            type: 'input',
-            required: true
-          }
-        ]
-      },
-      {
-        section: 'IC',
-        items: [
-          {
-            id: '5',
-            label: 'Imię i nazwisko',
-            type: 'input',
-            required: true
-          },
-          {
-            id: '6',
-            label: 'Narodowość',
-            type: 'input',
-            required: true
-          },
-          {
-            id: '7',
-            label: 'Wiek',
-            type: 'input',
-            required: true
-          },
-          {
-            id: '8',
-            label: 'Płeć',
-            type: 'input',
-            required: true
-          },
-          {
-            id: '9',
-            label: 'Czy byłeś karany, jak tak to za co?',
-            type: 'textarea',
-            required: true
-          },
-          {
-            id: '10',
-            label: 'Jaką szkołę ukończyłeś',
-            type: 'input',
-            required: true
-          },
-          {
-            id: '11',
-            label: 'Poprzednie doświadczenia we frakcji:',
-            type: 'textarea',
-            required: true
-          },
-          {
-            id: '12',
-            label: 'List motywacyjny: (min 100 słów)',
-            type: 'textarea',
-            required: true
-          }
-        ]
-      }
-    ]
-  },
-
-{
-  key: 'autoexotic',
-  name: 'Auto Exotic',
-  icon: '🔧',
-  color: '#f97316',
-  desc: 'Dołącz do ekipy profesjonalnych mechaników i rozwijaj swoją karierę motoryzacyjną.',
-
-  webhook: 'https://discord.com/api/webhooks/1506329960149946378/aEzWzMia00ZxzTSwkV35EjAAr7NpKpi13d8FVJxsJ3zf-x6nzpa4We_ZXk-k834VNglG',
-  roleId: '1480310820956016712',
-
-  questions: [
-    {
-      section: 'OOC',
-      items: [
-        {
-          id: '20030300303',
-          label: 'Wiek:',
-          type: 'input',
-          required: true,
-          maxLength: 2
-        },
-        {
-          id: '45464765',
-          label: 'Czy posiadasz mutację:',
-          type: 'input',
-          required: true
-        },
-      ]
-    },
-
-    {
-      section: 'IC',
-      items: [
-        {
-          id: '23123233333',
-          label: 'Wiek:',
-          type: 'input',
-          required: true
-        },
-        {
-          id: '56546453534',
-          label: 'Czy posiadasz prawo jazdy kat. B:',
-          type: 'input',
-          required: true
-        },
-        {
-          id: '6321321',
-          label: 'Napisz kilka zdań o sobie (minimum 5 zdań):',
-          type: 'textarea',
-          required: true
-        },
-        {
-          id: '7321321',
-          label: 'Poprzednie doświadczenie:',
-          type: 'textarea',
-          required: true
-        },
-        {
-          id: '8321312',
-          label: 'Jeśli pracowałeś na innych serwerach, podaj nazwę frakcji oraz zajmowany stopień:',
-          type: 'textarea',
-          required: false
-        },
-        {
-          id: '3123219',
-          label: 'Na jakim stopniu chciałbyś się znaleźć w przyszłości?',
-          type: 'textarea',
-          required: true
-        },
-        {
-          id: '1021121232',
-          label: 'Czy masz doświadczenie w pracy grupowej? Rozwiń swoją wypowiedź:',
-          type: 'textarea',
-          required: true
-        },
-        {
-          id: '111221',
-          label: 'Oceń swoją spostrzegawczość w skali od 1 do 5:',
-          type: 'input',
-          required: true
-        }
-      ]
-    }
-  ]
-},
 
    {
     key: 'test1',
@@ -415,79 +176,110 @@ const FACTIONS = [
       }
     ]
   },
-   {
-    key: 'test2',
-    name: 'CRIME',
-    icon: '💀',
-    color: '#f87171',
-    desc: 'Organizacja przestępcza. Buduj imperium i władaj ulicami od zera.',
 
-    webhook: 'https://discord.com/api/webhooks/1506329541235310602/yPzfVU3tGYHobc5-q-fibLGOEoZTWBr9wdfoXFpeVieJ_bHfaxDEbI273IVq19mxVB4O',
-    roleId: '1485660946725601310',
+  {
+    key: 'test', 
+    name: 'LSSD',
+    icon: '🛡️',
+    color: '#2dd4bf',
+    desc: "Los Santos Sheriff's Department. Ochrona spokoju poza miastem.",
+
+    webhook: 'https://discord.com/api/webhooks/1506332001198936098/U999ruVpvlqP5xpvpL3Bil5l0JRZtUPqAu8jSfEVuBGZ9tRfEteFGv-Vm6Vuxce4DzGe',
+    roleId: '1480310711878946916',
 
     questions: [
       {
         section: 'OOC',
         items: [
           {
-            id: '29',
-            label: 'Wiek:',
+            id: '2',
+            label: 'Wiek',
             type: 'input',
             required: true,
             maxLength: 2
           },
           {
-            id: '30',
-            label: 'Ile masz przegranych godzin w fivem:',
+            id: '3',
+            label: 'Imię',
             type: 'input',
             required: true
           },
           {
-            id: '31',
-            label: 'Nazwa organizacji:',
+            id: '4a',
+            label: 'Doświadczenie w frakcji LEA',
             type: 'input',
             required: true
           },
           {
-            id: '32',
-            label: 'Jaką organizacje chcesz założyć (Mafia, Gang uliczny):',
+            id: '4b',
+            label: 'Na jakich serwerach grałeś',
             type: 'input',
             required: true
           },
           {
-            id: '33',
-            label: 'Ile macie ludzi:',
-            type: 'input',
-            required: true
-          },
-          {
-            id: '34',
-            label: 'Opis organizacji:',
-            type: 'input',
-            required: true
-          },
-          {
-            id: '35',
-            label: 'Czym będzie się zajmować organizacja:',
-            type: 'input',
-            required: true
-          },
-          {
-            id: '36',
-            label: 'Link do interioru:',
-            type: 'input',
-            required: true
-          },
-          {
-            id: '37',
-            label: 'Link do dc organizacji:',
+            id: '4c',
+            label: 'Oceń swoją wiedze w skali od 1-10',
             type: 'input',
             required: true
           }
         ]
       },
+      {
+        section: 'IC',
+        items: [
+          {
+            id: '5',
+            label: 'Imię i nazwisko',
+            type: 'input',
+            required: true
+          },
+          {
+            id: '6',
+            label: 'Narodowość',
+            type: 'input',
+            required: true
+          },
+          {
+            id: '7',
+            label: 'Wiek',
+            type: 'input',
+            required: true
+          },
+          {
+            id: '8',
+            label: 'Płeć',
+            type: 'input',
+            required: true
+          },
+          {
+            id: '9',
+            label: 'Czy byłeś karany, jak tak to za co?',
+            type: 'textarea',
+            required: true
+          },
+          {
+            id: '10',
+            label: 'Jaką szkołę ukończyłeś',
+            type: 'input',
+            required: true
+          },
+          {
+            id: '11',
+            label: 'Poprzednie doświadczenia we frakcji:',
+            type: 'textarea',
+            required: true
+          },
+          {
+            id: '12',
+            label: 'List motywacyjny: (min 100 słów)',
+            type: 'textarea',
+            required: true
+          }
+        ]
+      }
     ]
   },
+
    {
     key: 'test3',
     name: 'EMS',
@@ -602,6 +394,217 @@ const FACTIONS = [
       }
     ]
   },
+
+{
+  key: 'autoexotic',
+  name: 'Auto Exotic',
+  icon: '🔧',
+  color: '#f97316',
+  desc: 'Dołącz do ekipy profesjonalnych mechaników i rozwijaj swoją karierę motoryzacyjną.',
+
+  webhook: 'https://discord.com/api/webhooks/1506329960149946378/aEzWzMia00ZxzTSwkV35EjAAr7NpKpi13d8FVJxsJ3zf-x6nzpa4We_ZXk-k834VNglG',
+  roleId: '1480310820956016712',
+
+  questions: [
+    {
+      section: 'OOC',
+      items: [
+        {
+          id: '20030300303',
+          label: 'Wiek:',
+          type: 'input',
+          required: true,
+          maxLength: 2
+        },
+        {
+          id: '45464765',
+          label: 'Czy posiadasz mutację:',
+          type: 'input',
+          required: true
+        },
+      ]
+    },
+
+    {
+      section: 'IC',
+      items: [
+        {
+          id: '23123233333',
+          label: 'Wiek:',
+          type: 'input',
+          required: true
+        },
+        {
+          id: '56546453534',
+          label: 'Czy posiadasz prawo jazdy kat. B:',
+          type: 'input',
+          required: true
+        },
+        {
+          id: '6321321',
+          label: 'Napisz kilka zdań o sobie (minimum 5 zdań):',
+          type: 'textarea',
+          required: true
+        },
+        {
+          id: '7321321',
+          label: 'Poprzednie doświadczenie:',
+          type: 'textarea',
+          required: true
+        },
+        {
+          id: '8321312',
+          label: 'Jeśli pracowałeś na innych serwerach, podaj nazwę frakcji oraz zajmowany stopień:',
+          type: 'textarea',
+          required: false
+        },
+        {
+          id: '3123219',
+          label: 'Na jakim stopniu chciałbyś się znaleźć w przyszłości?',
+          type: 'textarea',
+          required: true
+        },
+        {
+          id: '1021121232',
+          label: 'Czy masz doświadczenie w pracy grupowej? Rozwiń swoją wypowiedź:',
+          type: 'textarea',
+          required: true
+        },
+        {
+          id: '111221',
+          label: 'Oceń swoją spostrzegawczość w skali od 1 do 5:',
+          type: 'input',
+          required: true
+        }
+      ]
+    }
+  ]
+},
+
+   {
+    key: 'test2',
+    name: 'CRIME',
+    icon: '💀',
+    color: '#f87171',
+    desc: 'Organizacja przestępcza. Buduj imperium i władaj ulicami od zera.',
+
+    webhook: 'https://discord.com/api/webhooks/1506329541235310602/yPzfVU3tGYHobc5-q-fibLGOEoZTWBr9wdfoXFpeVieJ_bHfaxDEbI273IVq19mxVB4O',
+    roleId: '1485660946725601310',
+
+    questions: [
+      {
+        section: 'OOC',
+        items: [
+          {
+            id: '29',
+            label: 'Wiek:',
+            type: 'input',
+            required: true,
+            maxLength: 2
+          },
+          {
+            id: '30',
+            label: 'Ile masz przegranych godzin w fivem:',
+            type: 'input',
+            required: true
+          },
+          {
+            id: '31',
+            label: 'Nazwa organizacji:',
+            type: 'input',
+            required: true
+          },
+          {
+            id: '32',
+            label: 'Jaką organizacje chcesz założyć (Mafia, Gang uliczny):',
+            type: 'input',
+            required: true
+          },
+          {
+            id: '33',
+            label: 'Ile macie ludzi:',
+            type: 'input',
+            required: true
+          },
+          {
+            id: '34',
+            label: 'Opis organizacji:',
+            type: 'input',
+            required: true
+          },
+          {
+            id: '35',
+            label: 'Czym będzie się zajmować organizacja:',
+            type: 'input',
+            required: true
+          },
+          {
+            id: '36',
+            label: 'Link do interioru:',
+            type: 'input',
+            required: true
+          },
+          {
+            id: '37',
+            label: 'Link do dc organizacji:',
+            type: 'input',
+            required: true
+          }
+        ]
+      },
+    ]
+  },
+
+  {
+    key: 'podanienafirme', 
+    name: 'FIRMA',
+    icon: '🏬',
+    color: '#caa35e',
+    desc: "Załóż firmę tutaj!",
+
+    webhook: 'https://discord.com/api/webhooks/1509270202318192640/7WTh4gmZDL2MBwWkR8EyUf-GQ-bMAKrcjepngr7B5Q5e-EsWc8T9tKx7Py_ktlYWJv96',
+    roleId: '1480310121707077753',
+
+    questions: [
+      {
+        section: 'OOC',
+        items: [
+          {
+            id: 'firma1',
+            label: 'Ile masz lat',
+            type: 'input',
+            required: true,
+            maxLength: 2
+          },
+          {
+            id: 'firma2',
+            label: 'Ile masz przegranych godzin w fivem:',
+            type: 'input',
+            required: true,
+          },
+          {
+            id: 'firma3',
+            label: 'Ile posiadasz ludzi:',
+            type: 'input',
+            required: true,
+          },
+          {
+            id: 'firma4',
+            label: 'Jaka firma:',
+            type: 'input',
+            required: true,
+          },
+          {
+            id: 'firma5',
+            label: 'Plan na firmę:',
+            type: 'textarea',
+            required: true,
+          },
+        ]
+      },
+    ]
+  },
+
 ];
 
 const TEAM = [
