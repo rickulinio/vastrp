0f6d21c2f17a2476b2d4ae1260b3793f77d2bfae
rickulinio <rickulinio@gmail.com>
2026-05-27 22:29:48 +0200

    jhjh

---
+++ js/config.js
@@ -0,0 +1,13 @@
+const CONFIG = {
+    // ID graczy i przypisane im frakcje (role)
+    admins: {
+        "123456789012345678": ["lspd", "ems"],
+        "876543210987654321": ["crime"]
+    },
+    // Frakcje dostępne na serwerze
+    factions: {
+        "lspd": { name: "LSPD", status: true },
+        "ems": { name: "EMS", status: true },
+        "crime": { name: "Organizacja", status: true }
+    }
+};
